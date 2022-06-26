f9820d4b8a77ab83b9af08cde84d9c7f892bcc9a
senaygui <senaygeletaw@gmail.com>
2022-06-26 16:23:38 +0300

    update

--- app/models/student.rb
+++ app/models/student.rb
@@ -96,7 +96,7 @@ class Student < ApplicationRecord
       registration.program_name = self.program.program_name
       registration.admission_type = self.admission_type
       registration.study_level = self.study_level
-      registration.student_id_number = self.student.student_id
+      registration.student_id_number = self.student_id
       # registration.registrar_approval_status ="approved"
       # registration.finance_approval_status ="approved"
     end
