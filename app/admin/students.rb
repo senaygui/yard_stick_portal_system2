84499ebf65c5fdb5b98e9f272415f9e55324a89b
senaygui <senaygeletaw@gmail.com>
2022-07-22 19:30:01 +0300

    update

--- app/admin/students.rb
+++ app/admin/students.rb
@@ -389,19 +389,20 @@ ActiveAdmin.register Student do
         panel "Document" do
           attributes_table_for student do
             row "Documents" do |i|
-              div class: "document-preview container" do
-                i.documents.each do |doc| 
-                  if doc.variable?
-                    div class: "preview-card" do
-                      span link_to image_tag(doc, size: '200x200'), doc
-                    end
-                  elsif doc.previewable?
-                    div class: "preview-card" do
-                      span link_to "view document", doc.service_url
-                    end
-                  end
-                end
-              end
+              span link_to "view document", doc.service_url
+              # div class: "document-preview container" do
+              #   i.documents.each do |doc| 
+              #     if doc.variable?
+              #       div class: "preview-card" do
+              #         span link_to image_tag(doc, size: '200x200'), doc
+              #       end
+              #     elsif doc.previewable?
+              #       div class: "preview-card" do
+              #         span link_to "view document", doc.service_url
+              #       end
+              #     end
+              #   end
+              # end
             end
           end
         end
